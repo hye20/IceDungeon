816b4882abe593d30fa213b7c58bf21cacd6e3f5
NoelKang3313 <148937396+NoelKang3313@users.noreply.github.com>
2023-12-11 11:04:57 +0900

    update
    update

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -11,18 +11,12 @@ public class UIManager : MonoBehaviour
     public GameObject QuestPanel;
     public GameObject DicePanel;
 
-    public Animator BookAnimator;
-    public Button HelpExitButton;
-
     public Camera StarterCamera;
     EventManager eventManager;
 
     void Start()
     {
         eventManager = GameObject.Find("EventManager").GetComponent<EventManager>();
-
-        HelpButton.GetComponent<Button>().onClick.AddListener(OnHelpButtonClicked);
-        HelpExitButton.onClick.AddListener(OnHelpExitButtonClicked);
     }
     
     void Update()
@@ -45,20 +39,6 @@ public class UIManager : MonoBehaviour
 
                 Destroy(StarterCamera);
             }
-        }
-    }
-
-    void OnHelpButtonClicked()
-    {
-        eventManager.isAction = true;
-    }
-
-    void OnHelpExitButtonClicked()
-    {
-        if (BookAnimator.GetCurrentAnimatorStateInfo(0).IsName("HelpBook_Closed")
-            && BookAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
-        {
-            eventManager.isAction = false;
         }
     }
 }
